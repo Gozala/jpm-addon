037f6138010c77aafb73895a9facb1027d4e51c0
Irakli Gozalishvili <rfobic@gmail.com>
2015-04-03 12:00:59 -0700

    Switch over to shipped versions of loader / require and bootstrap

--- util.js
+++ util.js
@@ -3,8 +3,8 @@
 const writeBootstrap = (mountURI, manifest) =>
 `"use strict";
 const { utils: Cu } = Components;
-const {require} = Cu.import("resource://jpm/toolkit/require.js", {});
-const {Bootstrap} = require("resource://jpm/boot.js");
+const {require} = Cu.import("resource://gre/modules/commonjs/toolkit/require.js", {});
+const {Bootstrap} = require("resource://gre/modules/commonjs/sdk/addon/bootstrap");
 const {startup, shutdown, install, uninstall} = new Bootstrap("${mountURI}");
 `
 exports.writeBootstrap = writeBootstrap;
