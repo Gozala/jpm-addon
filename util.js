6b74bd5439c37a0d403ef4c2c62eba2a13ce81a1
Irakli Gozalishvili <rfobic@gmail.com>
2015-04-21 14:57:45 -0700

    Fix add-on export.

--- util.js
+++ util.js
@@ -1,10 +1,11 @@
 "use strict";
 
+const rootURI = `__SCRIPT_URI_SPEC__.replace("bootstrap.js", "src/")`;
 const writeBootstrap = (mountURI, manifest) =>
 `"use strict";
 const { utils: Cu } = Components;
 const {require} = Cu.import("resource://gre/modules/commonjs/toolkit/require.js", {});
 const {Bootstrap} = require("resource://gre/modules/commonjs/sdk/addon/bootstrap");
-const {startup, shutdown, install, uninstall} = new Bootstrap("${mountURI}");
+const {startup, shutdown, install, uninstall} = new Bootstrap(${mountURI ? `"${mountURI}"` : rootURI});
 `
 exports.writeBootstrap = writeBootstrap;
