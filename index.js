2a9661a5b79b4137a2fb74c819ff4d8fe0027c0a
Irakli Gozalishvili <rfobic@gmail.com>
2015-04-22 00:13:23 -0700

    Use gcli wrapper from sdk.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const gcli = require("./dev/gcli");
+const gcli = require("dev/gcli");
 const { Addon, ExistingDirectoryPath, mountAddon,
         reloadAddon, exportAddon, installAddon, uninstallAddon } = require("./core");
 
