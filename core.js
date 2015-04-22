e72557eb4babc23862dc26f38446eece3be4938b
Irakli Gozalishvili <rfobic@gmail.com>
2015-04-22 00:13:02 -0700

    Add license headers.

--- core.js
+++ core.js
@@ -1,3 +1,8 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
 const { Cc, Ci } = require("chrome");
 const { OS: {File}} = require("resource://gre/modules/osfile.jsm");
 const { Task } = require("resource://gre/modules/Task.jsm");
